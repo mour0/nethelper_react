62e17d52caf4dfaf4c0269fadc74d14b89cea16d
mour0 <malentacchimaurizio19@gmail.com>
2023-02-08 02:08:24 +0100

    background, text input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Store from '@mui/icons-material/Store';
+
+import Grid2 from '@mui/material/Unstable_Grid2';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+
+
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+
+const opts = [
+  {
+    value: 'v4Calc',
+    label: 'IPv4',
+  },
+  {
+    value: 'v4Mask',
+    label: 'IPv4 Mask',
+  },
+  {
+    value: 'v4VLMS',
+    label: 'VLMS',
+  },
+  {
+    value: 'v6Calc',
+    label: 'IPv6',
+  },
+  {
+    value: 'v6Mask',
+    label: 'IPv6 Mask',
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 0 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            NetHelper
+          </Typography>
+          <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                //onClick={handleMenu}
+                color="inherit"
+              >
+              <Store />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+
+      <Container maxWidth="sm" sx={{p: 4}}>
+      <Grid2 container spacing={2}>
+        <Grid2 xs={4}>
+          <TextField fullWidth
+            id="outlined-select-action"
+            select
+            label="Select Action"
+            defaultValue="v4Calc"
+            //helperText="Please select your action"
+          >
+            {opts.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Grid2>
+
+        <Grid2 xs={8}>
+          <TextField fullWidth id="outlined-basic" label="Outlined" variant="outlined" />
+        </Grid2>
+
+      </Grid2>
+      </Container>
+
+
+
+    </Box>
+
+
   );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -3,11 +3,92 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import amber from '@mui/material/colors/amber';
+import deepOrange from '@mui/material/colors/deepOrange';
+import grey from '@mui/material/colors/grey';
+
+//const getDesignTokens = (mode) => ({
+//  palette: {
+//    mode,
+//    ...(mode === 'light'
+//      ? {
+//          // palette values for light mode
+//          primary: amber,
+//          divider: amber[200],
+//          text: {
+//            primary: grey[900],
+//            secondary: grey[800],
+//          },
+//        }
+//      : {
+//          // palette values for dark mode
+//          primary: deepOrange,
+//          divider: deepOrange[700],
+//          background: {
+//            default: '#121212',
+//            paper: deepOrange[900],
+//          },
+//          text: {
+//            primary: '#fff',
+//            secondary: grey[500],
+//          },
+//        }),
+//  },
+//});
+//const darkThemeTEST = createTheme(getDesignTokens('dark'));
+
+const cLightBlue = {
+  50: '#e2fbff',
+  100: '#b6f5fe',
+  200: '#83eeff',
+  300: '#4ee6fd',
+  400: '#27dffa',
+  500: '#11d8f7',
+  600: '#0ac7e3',
+  700: '#02b1c7',
+  800: '#019dae',
+  900: '#007880',
+}
+
+
+const _cViolet = {
+  50: '#efe6fd',
+  100: '#d4c2f9',
+  200: '#b799f6',
+  300: '#986ef3',
+  400: '#7e4bf0',
+  500: '#632ae5',
+  600: '#5725df',
+  700: '#441cd6',
+  800: '#3016cd',
+  900: '#0007bf',
+}
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: cLightBlue[200],
+    },
+    background: {
+      default: '#121212',
+      paper: cLightBlue[200],
+    },
+  },
+});
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
